de5f546ed3b362f1e7690cb3259adf83e080e53e
Kremlin Huaman <kremlinhuamansantos@gmail.com>
2024-11-23 17:02:19 +0000

    23nov

---
+++ Sesion2/ETL/1.retail/config.py
@@ -0,0 +1,19 @@
+
+DATABASE_CONFIG = {
+    'host': '10.0.10.31',
+    'port': 3310,
+    'user': 'root',
+    'password': 'root',
+    'database': 'retail_db'
+}
+
+CSV_FILES = {
+    'customers': 'data_retail/customers',
+    'departments': 'data_retail/departments',
+    'categories': 'data_retail/categories',
+    'products': 'data_retail/products',
+    'orders': 'data_retail/orders',
+    'order_items': 'data_retail/order_items',
+}
+
+LOG_FILE = 'logs/pipeline.log'
